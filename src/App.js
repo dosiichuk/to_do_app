135f0da4792caed97370f5e5a93b6e0eec5408ec
Dmytro <dmytro.osiychuk@gmail.com>
2022-01-17 13:38:34 +0100

    Create store, reducer and refactor for centralized state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Hero from "./components/Hero/Hero";
-import SearchForm from "./components/SearchForm/SearchForm";
-import List from "./components/List/List";
-import Container from "./components/Container/Container";
+import Hero from './components/Hero/Hero';
+import SearchForm from './components/SearchForm/SearchForm';
+import List from './components/List/List';
+import Container from './components/Container/Container';
 
 const App = () => {
   return (
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,26 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
-const Column = props => {
-    return (
-        <article className={styles.column}>
-            <h2 className={styles.title}><span className={`${styles.icon} fa fa-${props.icon}`}></span>{props.title}</h2>
-            <ul className={styles.cards}>
-                {props.cards.map(card => <Card key={card.id}>{card.title}</Card>)}
-            </ul>
-            <CardForm columnId={props.id} action={props.action} />
-        </article>
-    )
-}
+const Column = (props) => {
+  const cards = useSelector((state) =>
+    state.cards.filter((card) => card.columnId === props.id)
+  );
+  return (
+    <article className={styles.column}>
+      <h2 className={styles.title}>
+        <span className={`${styles.icon} fa fa-${props.icon}`}></span>
+        {props.title}
+      </h2>
+      <ul className={styles.cards}>
+        {cards.map((card) => (
+          <Card key={card.id}>{card.title}</Card>
+        ))}
+      </ul>
+      <CardForm columnId={props.id} action={props.action} />
+    </article>
+  );
+};
 
 export default Column;
